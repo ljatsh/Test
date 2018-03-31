a86025282bcdf75953411ceb5465b20d8c5f31c6
Longjun Wang <longjun.wang@qq.com>
2018-03-31 15:51:58 +0800

    Redirect after POST, and show all items in template

--- python/src/book/tdd_with_python/superlists/lists/tests.py
+++ python/src/book/tdd_with_python/superlists/lists/tests.py
@@ -11,11 +11,30 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_can_save_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new item'})
-        self.assertIn('A new item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
 
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item')
+
+    def test_can_redicrect_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new item'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/')
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
