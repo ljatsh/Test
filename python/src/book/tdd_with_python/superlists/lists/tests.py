7a51de12f3b201c64c8b80356b43319a784b4c44
Longjun Wang <longjun.wang@qq.com>
2018-03-31 11:56:58 +0800

    basic view content unittest

--- python/src/book/tdd_with_python/superlists/lists/tests.py
+++ python/src/book/tdd_with_python/superlists/lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from lists.views import home_page
 
 class SmokeTest(TestCase):
@@ -8,3 +9,10 @@ class SmokeTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_content(self):
+        request = HttpRequest()
+        response = home_page(request)
+        content = response.content.decode()
+        self.assertTrue(content.startswith('<html>'))
+        self.assertIn('<title>TO-DO lists</title>', content)
+        self.assertTrue(content.endswith('</html>'))
