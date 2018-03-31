c2aad97068571be023d0837abc5e351bb2079f7d
Longjun Wang <longjun.wang@qq.com>
2018-03-31 15:19:39 +0800

    home page can accept POST request

--- python/src/book/tdd_with_python/functional_test.py
+++ python/src/book/tdd_with_python/functional_test.py
@@ -11,6 +11,12 @@ class FunctionalTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_row_in_list_table(self, row_text):
+        to_do_table = self.browser.find_element_by_id('id_to_do_list')
+        rows = to_do_table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows],
+                      f'New to-do item does not appear. Contents were:\n{to_do_table.text}')
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # I heard about a cool new onlin to-do app and then, went to check its home page.
         self.browser.get('http://localhost:8000')
@@ -27,19 +33,24 @@ class FunctionalTest(unittest.TestCase):
 
         # The page updated after I hit enter. And now the page lists:
         # "1. call my brother today" as an item in the to-do list
-        text_box.send_keys(Keys.Enter)
+        text_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        to_do_table = self.browser.find_element_by_id('id_to_do_list')
-        rows = to_do_table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1. call my brother today' for row in rows))
+        self.check_row_in_list_table('1. call my brother today')
 
         # There was still an text box inviting me to enter another to-do item. I entered 'write today's diary'
-        self.fail('Finished')
+        # The DOM has been updated, so we need to pickup relevant elements again.
+        text_box = self.browser.find_element_by_id('id_new_item')
+        text_box.send_keys('write today\'s diary')
+        text_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updated again, and now both items were shown on the lists
+        self.check_row_in_list_table('1. call my brother today')
+        self.check_row_in_list_table('2. write today\'s diary')
 
         # I noticed the site had generated a unique URL for me -- there was some explanatory text to that effect.
+        self.fail('Finished')
 
         # I visited that URL and checked my to-do list was still there
 
--- python/src/book/tdd_with_python/superlists/lists/tests.py
+++ python/src/book/tdd_with_python/superlists/lists/tests.py
@@ -7,3 +7,8 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new item'})
+        self.assertIn('A new item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
