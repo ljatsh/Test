c6c68a7304fe95343d95a0c5748a52b5594ddf98
Longjun Wang <longjun.wang@qq.com>
2018-04-01 21:55:33 +0800

    test organization updates

--- js/src/book/learning_javascript_3rd_edition/cp4.js
+++ js/src/book/learning_javascript_3rd_edition/cp4.js
@@ -1,39 +1,8 @@
 // Learning JavaScript, 3rd Edition.
-// Chapter 4. Expressions and Operators
+// Chapter 4. Control Flow
 
 var assert = require('assert');
 
-// 1. Perfer === and !== to avoid unexpected behavior
-describe('Expressions and Operators', function() {
-    it('Equality and Inequality Operators', function() {
-        // strict equality
-        //// strict equality does not perform type conversations
-        assert.ok('1' !== 1, 'different types does not strict equal')
-        assert.ok(true !== 1, 'different types does not strict equal')
+describe('Control Flow', function() {
 
-        //// null, undefined and NaN
-        assert.ok(null === null)
-        assert.ok(undefined === undefined)
-        assert.ok(null !== undefined)
-        assert.ok(NaN !== NaN, 'NaN does not strict equal to any value, including itself')
-        assert.ok(NaN !== 'a', 'NaN does not strict equal to any value, including itself')
-
-        //// 0 and -0 are strict equal
-        var obj1 = {x:1}, obj2 = {x:1}
-        assert.ok(5 === 5)
-        assert.ok(0 === -0)
-        assert.ok(obj1 !== obj2, 'different object does not strict equal, event if they have the same properties')
-
-        // equality operator
-        //// type coversattion may occur bewteen different value types
-        assert.ok('1' == 1, 'convert string to number')
-        assert.ok(true == 1 && false == 0, 'convert boolean to number(true-->1, false-->0) firstly, and then check equality again')
-        assert.ok(true == '1' && false == '0', 'convert boolean to number(true-->1, false-->0) firstly, and then check equality again')
-
-        //// Object vs number/string/ is too complicated.
-        assert.ok(obj1 != obj2, 'TODO')
-
-        //// null and undefined are equal
-        assert.ok(null == undefined)
-    });
 });
