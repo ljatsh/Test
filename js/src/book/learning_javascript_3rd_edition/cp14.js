831794dcbe5b84b0818904c02b47dea66d0cc9a0
Longjun Wang <longjun.wang@qq.com>
2018-04-16 17:58:07 +0800

    Promise Practice and Serveral UnitTest tools Practice

--- js/src/book/learning_javascript_3rd_edition/cp14.js
+++ js/src/book/learning_javascript_3rd_edition/cp14.js
@@ -2,32 +2,130 @@
 // Learning JavaScript, 3rd Edition.
 // Chapter 4. Asynchronous Programming
 
-//var assert = require('assert');
-//var fs = require('fs');
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+var assert = chai.assert;
+
+// Promise:
+//   Gurantee:
+//   1. Callback will never to be called before the completion of the current run of the JavaScript event loop.
+//   2. Callback added withn then can be called even the promise was settled.
+//   3. Multile callbacks can be added with then several times to be executed independently in insertion order
+
+//   Chain:
+//   1. Returned non-promise value from the callback will be propogated.
+//   2. Promise value of the returned resolved promise from the callback will be propogated.
+//   3. If callback returns a pending promise, resolution/rejection of the new created promise will be subsequent
+//      to the resolution/rejection of the pending promise.
+//   4. Error Propogation in Promise Chain just like catch clause.
 
 describe('Asynchronous Programming', function() {
-    it('Promise', function() {
+    describe('Promise Test', function() {
+        it('Promise Signature', function(done) {
+            var promise = new Promise((resolve, reject) => {
+                assert.isFunction(resolve);
+                assert.isFunction(reject);
 
-        // var promise = new Promise(function(resolve, reject) {
-        //     console.log(resolve);
-        //     console.log(reject);
-        //     //setTimeout(resolve, 1, 'foo');
-        //     resolve('foo');
-        // });
+                resolve();
+            });
 
-        // setTimeout(()=>promise.then((msg)=>{console.log('done...', msg);}),
-        //            10);
+            promise.then(done);
+        });
 
-        // promise.then((msg)=>{ console.log('done', msg); })
-        //        .then((msg)=>{ console.log('done2', msg); })
-        //        .catch((error)=>{console.log(error);});
+        // If the callback handler returns a value, the created promise will be resolved with the returned value as its value
+        it('Then method ---> 1', function(done) {
+            Promise.resolve(3)
+                   .then((value) => { return value * 2; })
+                   .then((value) => {
+                        assert.equal(value, 6);
+                        done();
+                   });
+        });
 
-        // console.log('Promise ended');
-    });
+        // If the callback handler returns a resolved promise, the created promise will be resolved with that promise's value as its value
+        it('Then method ---> 2', function(done) {
+            var promise = Promise.resolve(3);
+            promise.then(()=> { return promise; })
+                   .then((value)=> {
+                        assert.equal(value, 3);
+                        done();
+                   });
+        });
 
-    it('Mocha', function(done) {
-        console.log(done);
-        setImmediate(done);
-        setImmediate(done);
+        // If the callback handler returns a pending promise, the resolution/rejection of the created promise will be
+        // subsequent to the resolution/rejection of that pending promise.
+        it('Then method ---> 3', function(done) {
+            var data = [];
+
+            var promise = new Promise((resolve) => {
+                setImmediate(()=> { resolve(3); });
+            });
+            promise.then((value)=> { data.push(value); });
+
+            Promise.resolve(0)
+                   .then(()=> { return promise; })
+                   .then((value)=> {
+                        assert.equal(value, 3);
+                        assert.deepEqual(data, [3]);
+                        done();
+                   });
+        });
+
+        // Callback will never to be called before the completion of the current run of the JavaScript event loop
+        it('Promise Gurantee ---> 1', function() {
+            var data = [];
+            var promise = Promise.resolve().then(()=> { assert.deepEqual(data, [2]); });
+            data.push(2);
+
+            return promise;
+        });
+
+        // Callback added with then can be called even the promise was settled
+        // multile callbacks can be added with then several times to be executed independently in insertion order
+        it('Promise Gurantee ---> 2', function(done) {
+            var data = [];
+            var promise = Promise.resolve(1);
+
+            promise.then((message)=> { data.push(message); });
+            promise.then((message)=> { data.push(message * 2); });
+            process.nextTick(function() { promise.then((message)=>{ assert.deepEqual(data, [1, 2]);
+                                                                    assert.equal(message, 1);
+                                                                    done();
+                                                                  });
+                                        });
+        });
+
+        it('Error Propogation', function(done) {
+            var data = [];
+            var catched = false;
+            Promise.reject(100)
+                   .then(()=> { data.push(1); })
+                   .then(()=> { data.push(2); })
+                   .catch((reason)=> {
+                        assert.equal(data.length, 0);
+                        assert.equal(reason, 100);
+                        catched = true;
+                   })
+                   .then((value)=> {
+                                     assert.isUndefined(value);
+                                     return Promise.resolve(1);
+                                   })
+                   .then((value)=> {
+                                     data.push(value);
+                                     return value;
+                                    })
+                   .then((value)=> {
+                                     throw 99;
+                   })
+                   .catch((reason)=> {
+                                     assert.isTrue(catched);
+                                     assert.deepEqual(data, [1]);
+                                     assert.equal(reason, 99);
+                                     done();
+                   });
+        });
     });
 });
--- js/src/packages/mocha_test.js
+++ js/src/packages/mocha_test.js
@@ -2,35 +2,106 @@
 // Mocha Practice
 // https://mochajs.org
 
-var assert = require('assert');
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
 
+chai.use(chaiAsPromised);
+
+var assert = chai.assert;
+
+// 1. Arrow Funtion to Mocha is discouraged. 
 describe('MochaTest', function() {
+    this.retries(4);
+    this.slow(10000);
+
+    // Hooks
+    before(function() {
+        console.log("before");
+    });
+
+    after(function() {
+        console.log("after");
+    });
+
+    beforeEach(function() {
+        console.log("beforeEach ---> no parameters");
+    });
+
+    beforeEach("step1", function() {
+        console.log("beforeEach ---> step1");
+    });
+
+    beforeEach("step2", function() {
+        console.log("beforeEach ---> step2");
+    });
+
+    beforeEach("asyncStep1", function(done) {
+        setImmediate(()=>{
+            console.log("beforeEach ---> asyncStep1");
+            done();
+        });
+    });
+
+    afterEach("asyncStep1", function(done) {
+        setImmediate(()=>{
+            console.log("afterEach ---> asyncStep1");
+            done();
+        });
+    });
+
+    afterEach("step2", function() {
+        console.log("afterEach ---> step2");
+    });
+
+    afterEach("step1", function() {
+        console.log("afterEach ---> step1");
+    });
+
+    afterEach(function() {
+        console.log("afterEach ---> no parameters");
+    });
+
+
     // Add 1 extra parameter to it to let mocha wait for the callback to be called to complete
     // the test. 
     // Because the 1st parameter in callback is error, so falsy value should pass the test,
     // anything else will cause a failed test.
     it('Asynchronous Test --- Success', function(done) {
-        assert.equal(typeof done, 'function');
+        assert.isFunction(done);
 
         setImmediate(done);
     });
 
-    it('Asynchronous Test --- Error', function(done) {
+    it.skip('Asynchronous Test --- Error', function(done) {
         setImmediate(done, new Error());
     });
 
-    // // Promise 'Return' cannot be with callback together
-    // it('Promise Test 1', function() {
-    //     return new Promise((resolve) => {
-    //         assert.ok(true);
-    //         resolve();
-    //     });
-    // });
+    // Promise 'Return' cannot be with callback together
+    it('Promise Test --- Success', function() {
+        return new Promise((resolve) => {
+            assert.ok(true);
+            resolve();
+        });
+    });
 
-    // it('Promise Test 2', function(done) {
-    //     new Promise((resolve) => {
-    //         assert.ok(true);
-    //         resolve(true);
-    //     }).then(done);
+    it.skip('Promise Test --- Error', function(done) {
+        return new Promise((resolve) => {
+            assert.ok(true);
+            resolve();
+        }).then(done);
+    });
+
+    it('Delayed Root Suite --- TODO');
+
+    it('Duration', function() {
+        assert.isOk(true);
+    });
+
+    // TODO. Arrow Notation
+    // describe('', ()=> {
+    //     it('Arrow Functions', ()=> {
+    //         this.timeout(1000);
+    //         console.log(typeof this);
+    //     });
     // });
 });
