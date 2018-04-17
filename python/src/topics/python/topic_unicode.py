4e171fb5ce9aaa2b1f944c426f2bf33611b54940
Longjun Wang <longjun.wang@qq.com>
2018-04-17 20:46:56 +0800

    unicode practice in python

---
+++ python/src/topics/python/topic_unicode.py
@@ -0,0 +1,39 @@
+
+# -*-: utf-8 -*-
+
+"""
+Reference: http://jkorpela.fi/chars.html
+           https://docs.python.org/3/howto/unicode.html
+
+1. Character repertoire ---> Character sets. A set of distinct characters for human reading.
+2. Character code ---> A mapping which defines a one-to-one correspondence between characters and a set of
+   non negative integers.(aka coding point, code number)
+3. Character coding ---> A method for presenting characters in digital form by mapping sequences of code points
+   of characters into sequences of octets. One purpose of encoding is to save space.
+
+4. Code point is independent with coding algorithm.
+
+"""
+
+import unittest
+
+
+class UnicodeTest(unittest.TestCase):
+
+    def test_code_point(self):
+        self.assertEqual('A', chr(ord('A')))
+
+    def test_unicode_literal(self):
+        self.assertEqual('\u4e2d\u534e\u4eba\u6c11\u5171\u548c\u56fd', '中华人民共和国')
+
+    def test_encoding(self):
+        b = '中华人民共和国'.encode('utf-8')
+        self.assertEqual(b.decode('utf-8'), '中华人民共和国')
+
+    def test_codecs_124(self):
+        # TODO
+        pass
+
+if __name__ == '__main__':
+    # print([hex(ord(c)) for c in '中华人民共和国']);
+    unittest.main(verbosity=2)
