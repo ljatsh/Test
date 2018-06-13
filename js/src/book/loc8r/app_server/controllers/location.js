10a45a3613e39a537e609ca1c52b08e1a615f8b6
ljatsh <longjun@192.168.0.102>
2018-06-13 08:22:19 +0800

    数据和视图初步分离

--- js/src/book/loc8r/app_server/controllers/location.js
+++ js/src/book/loc8r/app_server/controllers/location.js
@@ -1,10 +1,44 @@
 
 module.exports.home = function(req, res) {
-  res.render('location_list', { title: 'Home' });
+  res.render('location_list', { 
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    },
+    locations: [{
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+      },{
+        name: 'Cafe Hero',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '200m'
+      },{
+        name: 'Burger Queen',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 2,
+        facilities: ['Food', 'Premium wifi'],
+      distance: '250m'
+    }]    // end of location
+   });
 };
 
 module.exports.locationInfo = function(req, res) {
-  res.render('location_info', { title: 'Location Info' });
+  res.render('location_info', {
+    title: 'Location Info',
+    location: {
+      name: 'Burger Queen',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 2,
+      facilities: ['Food', 'Premium wifi'],
+      distance: '250m'
+    } // end of location
+  });
 };
 
 module.exports.addReview = function(req, res) {
