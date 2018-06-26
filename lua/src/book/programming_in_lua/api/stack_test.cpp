43372848c277d8ecc87be1d0b73f5ac447f9faa1
lj@sh <longjun.wang@qq.com>
2018-06-26 21:05:59 +0800

    lua stack practice

--- lua/src/book/programming_in_lua/api/stack_test.cpp
+++ lua/src/book/programming_in_lua/api/stack_test.cpp
@@ -25,8 +25,45 @@ class LuaTest : public ::testing::Test {
 };
 
 TEST_F(LuaTest, TestStack) {
-  lua_pushnil(_Lua);
+  size_t len_ = 8;
+  char* szBuffer = static_cast<char*>(std::malloc(len_));
+  szBuffer[len_ - 1] = '\0';
+  std::strncpy(szBuffer, "lj@sh", len_ - 1);
 
-  ASSERT_EQ(1, lua_gettop(_Lua)) << "stack size is 1";
-  ASSERT_TRUE(lua_isnil(_Lua, 1) && lua_isnil(_Lua, -1));
+  lua_pushnumber(_Lua, 1.2f);     // 1
+  lua_pushinteger(_Lua, 5);       // 2
+  lua_pushboolean(_Lua, 2);       // 3
+  lua_pushnil(_Lua);              // 4
+
+  // lua_pushstring create a local copy in lua. It is safe to release c string.
+  const char* s_ = lua_pushstring(_Lua, szBuffer); // 5
+  ASSERT_STRCASEEQ(szBuffer, s_);
+  ASSERT_NE(szBuffer, s_);
+  std::memset(szBuffer, 0, len_ - 1);
+  free(szBuffer);
+  ASSERT_STREQ("lj@sh", s_);
+
+  // Check
+  ASSERT_EQ(5, lua_gettop(_Lua)) << "check stack size";
+
+  // index 1
+  ASSERT_TRUE(lua_isnumber(_Lua, 1)) << "check 1";
+  ASSERT_FLOAT_EQ(1.2f, lua_tonumber(_Lua, 1));
+  
+  // index 2
+  ASSERT_TRUE(lua_isnumber(_Lua, 2)) << "check 2";
+  ASSERT_EQ(5, lua_tonumber(_Lua, 2));
+
+  // index 3
+  //ASSERT_TRUE(lua_isnumber(_Lua, 3)) << "check 3";
+  ASSERT_TRUE(lua_isboolean(_Lua, 3));
+  ASSERT_TRUE(lua_toboolean(_Lua, 3));
+
+  // index 4
+  ASSERT_TRUE(lua_isnil(_Lua, 4)) << "check 4";
+
+  // index 5
+  ASSERT_TRUE(lua_isstring(_Lua, -1)) << "check 5";
+  const char* s2_ = lua_tostring(_Lua, -1);
+  ASSERT_EQ(s_, s2_);
 }
