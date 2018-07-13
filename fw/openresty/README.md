d6372d8a49585dfea8dfcf6069fc1b72bd305b4a
lj@sh <longjun.wang@qq.com>
2018-07-13 16:02:36 +0800

    添加Test::Nginx的安装说明

---
+++ fw/openresty/README.md
@@ -0,0 +1,17 @@
+
+
+Test::Nginx的安装
+----------------
+
+### dependency
+
+```bash
+yum install -y which cpan make gcc
+```
+### install
+
+```bash
+cpan -f -i Test::Nginx
+```
+
+
--- fw/openresty/create_docker.sh
+++ fw/openresty/create_docker.sh
@@ -4,5 +4,6 @@ dir=$(cd `dirname $0`; pwd)
 
 docker run --name openresty --detach \
 -v $dir/conf.d:/etc/nginx/conf.d \
+-v $dir:/opt/dev \
 -p 8010:80 \
 openresty/openresty:centos
