e50eab8dd2ffb1eefa39c0993deca3d70e1f1feb
ljatsh <longjun.wang@qq.com>
2018-08-01 08:34:54 +0800

    resty-busted取代luaunit

--- fw/openresty/lua_test.lua
+++ fw/openresty/lua_test.lua
@@ -3,6 +3,5 @@ lu = require('luaunit')
 
 TestCJson = require('lua_cjson_test')
 TestMysql = require('lua_mysql_test')
-TestRedis = require('lua_redis_test')
 
 lu.LuaUnit.run()
--- fw/openresty/spec/resty_redis_spec.lua
+++ fw/openresty/spec/resty_redis_spec.lua
@@ -1,103 +1,100 @@
 
-local lu = require('luaunit')
 local json = require('cjson')
 local redis = require('resty.redis')
 
-local TestRedis = {}
+describe('redis', function()
+  local self = {}
 
-  function TestRedis:setUp()
+  setup(function()
     self.redis = redis:new()
     self.redis:set_timeout(1000)
 
     local ok, err = self.redis:connect('172.17.0.5', 6379)
-    lu.assertTrue(not not ok, err)
-  end
+    assert.truthy(ok, err)
+  end)
 
-  function TestRedis:tearDown()
-  end
+  teardown(function()
+    local ok, err = self.redis:set_keepalive(5000, 100)
+    assert.are.equal(ok, 1, err)
+  end)
 
-  function TestRedis:tearMissingKeyValue()
+  it('testMissingKeyValue', function()
     local res, err = self.redis:get('visits:0:totals')
-    lu.assertEquals(res, ngx.null, 'value of the missing key is ngx.null')
-  end
+    assert.are.equal(res, ngx.null, 'value of the missing key is ngx.null')
+  end)
 
-  function TestRedis:testCounter()
+  it('testCounter', function()
     self.redis:del('visits:1:totals')
 
     local ok, err = self.redis:set('visits:1:totals', 100)
-    lu.assertEquals('OK', ok, err)
+    assert.are.equal('OK', ok, err)
 
     local res, err = self.redis:get('visits:1:totals')
-    lu.assertEquals(res, '100', 'the value by set should be string')
+    assert.are.equal(res, '100', 'the value by set should be string')
 
     res, err = self.redis:incr('visits:1:totals')
-    lu.assertEquals(res, 101, 'incr returns integer')
+    assert.are.equal(res, 101, 'incr returns integer')
 
     res, err = self.redis:decr('visits:1:totals')
-    lu.assertEquals(res, 100, 'decr also returns integer')
+    assert.are.equal(res, 100, 'decr also returns integer')
 
     res, err = self.redis:incrby('visits:1:totals', 10)
-    lu.assertEquals(res, 110, 'incrby returns integer')
+    assert.are.equal(res, 110, 'incrby returns integer')
 
     res, err = self.redis:incrby('visits:1:totals', "5")
-    lu.assertEquals(res, 115, 'incrby params can be string')
+    assert.are.equal(res, 115, 'incrby params can be string')
 
     res, err = self.redis:incrby('visits:1:totals', -10)
-    lu.assertEquals(res, 105, 'negative param')
+    assert.are.equal(res, 105, 'negative param')
 
     res, err = self.redis:del('visits:1:totals')
-    lu.assertEquals(res, 1, err) -- 1 key should be removed
-  end
+    assert.are.equal(res, 1, err) -- 1 key should be removed
+  end)
 
-  function TestRedis:testHashset()
+  it('testHashset', function()
     self.redis:del('users:lj')
 
     local res, err = self.redis:hset('users:lj', 'name', 'ljatsh')
-    lu.assertEquals(1, res, 'field name was created')
+    assert.are.equal(1, res, 'field name was created')
     res, err = self.redis:hset('users:lj', 'age', 34)
-    lu.assertEquals(1, res, 'field age was created')
+    assert.are.equal(1, res, 'field age was created')
 
     -- hget
     res, err = self.redis:hget('users:lj', 'name')
-    lu.assertEquals(res, 'ljatsh')
+    assert.are.equal(res, 'ljatsh')
     res, err = self.redis:hget('users:lj', 'age')
-    lu.assertEquals(res, '34', 'hget returns string')
+    assert.are.equal(res, '34', 'hget returns string')
 
     -- hdel
     res, err = self.redis:hdel('users:lj', 'missing')
-    lu.assertEquals(0, res, '0 fields was removed')
+    assert.are.equal(0, res, '0 fields was removed')
 
     res, err = self.redis:hset('users:lj', 'missing', 'missing')
     res, err = self.redis:hdel('users:lj', 'missing')
-    lu.assertEquals(1, res)
+    assert.are.equal(1, res)
 
     res, err = self.redis:hexists('users:lj', 'missing')
-    lu.assertEquals(res, 0)
+    assert.are.equal(res, 0)
 
     res, err = self.redis:hexists('users:lj', 'age')
-    lu.assertEquals(res, 1)
+    assert.are.equal(res, 1)
 
     -- hkeys
     res, err = self.redis:hkeys('users:lj')
-    lu.assertEquals(#res, 2)
+    assert.are.equal(#res, 2)
 
     -- hvals
     res, err = self.redis:hvals('users:lj')
-    lu.assertEquals(#res, 2)
+    assert.are.equal(#res, 2)
 
     -- hgetall
     res, err = self.redis:hgetall('users:lj')
-    lu.assertEquals(#res, 4)
+    assert.are.equal(#res, 4)
 
     self.redis:del('users:lj')
-  end
+  end)
 
-  function TestRedis:testSets()
+  it('testSets', function()
 
-  end
-
-  
-
--- end of TestRedis
-
-return TestRedis
+  end)
+end)
