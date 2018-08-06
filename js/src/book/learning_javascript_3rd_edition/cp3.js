535c4a81f31f1e057b138355bedc27845c9d9d09
lj@sh <longjun.wang@qq.com>
2018-08-06 16:32:48 +0800

    refactor js tests with BDD style

--- js/src/book/learning_javascript_3rd_edition/cp3.js
+++ js/src/book/learning_javascript_3rd_edition/cp3.js
@@ -3,126 +3,127 @@
 // Learning JavaScript, 3rd Edition.
 // Chapter 3. Literals and Variables
 
-var assert = require('assert');
+var chai = require('chai');
+var expect = chai.expect;
 
-describe('Primitive types and Objects', function() {
-    // 1. Number.isNaN is different from global isNan. Number.isNaN determines whether the passed value is NaN and its type is Number. While global isNaN converts the passed value to Number.
-    // 2. Both equality operators, == and ===, evaluating to false when checking NaN is Nan. This is why Number.isNaN() becomes necessary.
-    describe('Number', function() {
-        it('NaN', function() {
-            assert.notEqual(Number.isNaN, isNaN, 'global isNan is different from Number.isNaN');
+describe('Primitive types and Objects', function () {
+  // 1. Number.isNaN is different from global isNan. Number.isNaN determines whether the passed value is NaN and its type is Number. While global isNaN converts the passed value to Number.
+  // 2. Both equality operators, == and ===, evaluating to false when checking NaN is Nan. This is why Number.isNaN() becomes necessary.
+  describe('Number', function () {
+    it('NaN', function () {
+      expect(Number.isNan, 'global isNan is different from Number.isNaN').to.be.not.equal(isNaN);
 
-            // 'NaN', undefined, {}, 'blabla' should be true with global isNaN
-            assert.ok(!Number.isNaN('NaN'));
-            assert.ok(!Number.isNaN(undefined));
-            assert.ok(!Number.isNaN({}));
-            assert.ok(!Number.isNaN('blabla'));
+      // 'NaN', undefined, {}, 'blabla' should be true with global isNaN
+      expect(Number.isNaN('NaN')).to.be.false;        // jshint ignore: line
+      expect(Number.isNaN(undefined)).to.be.false;    // jshint ignore: line
+      expect(Number.isNaN({})).to.be.false;           // jshint ignore: line
+      expect(Number.isNaN('blabla')).to.be.false;     // jshint ignore: line
 
-            assert.ok(isNaN('NaN'));
-            assert.ok(isNaN(undefined));
-            assert.ok(isNaN({}));
-            assert.ok(isNaN('blabla'));
+      expect(isNaN('NaN')).to.be.true;                // jshint ignore: line
+      expect(isNaN(undefined)).to.be.true;            // jshint ignore: line
+      expect(isNaN({})).to.be.true;                   // jshint ignore: line
+      expect(isNaN('blabla')).to.be.true;             // jshint ignore: line
 
-            assert.ok(!(NaN == NaN), 'Nan == Nan is false'); // jshint ignore: line
-            assert.ok(!(NaN === NaN), 'Nan === Nan is false'); // jshint ignore: line
+      expect(NaN == NaN, 'Nan == Nan is false').to.be.false;        // jshint ignore: line
+      expect(NaN, 'Nan === Nan is false').to.be.not.equal(NaN);     // jshint ignore: line
 
-            assert.ok(Number.isNaN(NaN));
-            assert.ok(Number.isNaN(Number.NaN));
-            assert.ok(Number.isNaN(0/0));
-        });
-
-        it('Infinity', function() {
-            assert.ok(!Number.isFinite(1/0));
-        });
-
-        it('Convered from string', function() {
-            assert.equal(Number.parseInt('5.6'), 5, 'numbers after dot are omitted');
-            assert.equal(Number.parseInt(' 5 '), 5, 'leading spaces are ignored');
-            assert.equal(Number.parseInt('ab', 16), 0xab, 'hexdecimal parsing');
-            assert.ok(Number.isNaN(Number.parseInt('ab')));
-            assert.equal(Number.parseInt, parseInt, 'global parseInt is alias of Number.parseInt');
-
-            assert.equal(1.5, Number.parseFloat('1.5'));
-            assert.equal(1.5, Number.parseFloat('1.5a'), 'first non digit character and remain ones are omitted');
-            assert.equal(Number.parseFloat, parseFloat, 'global parseFloat is alias of Number.parseFloat');
-        });
-
-        it('Converts to string', function() {
-            assert.equal((16.7).toString(), '16.7', 'default radix is 10');
-            assert.equal((16).toString(16), '10', 'hexdecimal format');
-            assert.equal((-16).toString(2), '-10000', 'binary format');
-        });
-
-        it('The valueOf', function() {
-            var numObj = new Number(11); // jshint ignore: line
-            var num = numObj.valueOf();
-            assert.equal(num, 11);
-            assert.equal(typeof(numObj), 'object');
-            assert.equal(typeof(num), 'number');
-        });
+      expect(Number.isNaN(NaN)).to.be.true;                         // jshint ignore: line
+      expect(Number.isNaN(Number.NaN)).to.be.true;                  // jshint ignore: line
+      expect(Number.isNaN(0 / 0)).to.be.true;                       // jshint ignore: line
     });
 
-    // 1. undefined, null, NaN, 0, '', false is falsy. Anything else is truthy
-    describe('Boolean', function() {
-        it('The falsy and truthy', function() {
-            assert.ok(!undefined);
-            assert.ok(!null);
-            assert.ok(!NaN);
-            assert.ok(!0);
-            assert.ok(!'');
-            assert.ok(!false);
-
-            assert.ok(new Boolean(false), 'any object is truthy, including objects whose valueOf returns false'); // jshint ignore: line
-            assert.ok([], 'array is truthy, event it is empty');
-            assert.ok(' ', 'non empty string is truthy');
-            assert.ok('false');
-        });
+    it('Infinity', function () {
+      expect(Number.isFinite(1 / 0)).to.be.false;     // jshint ignore: line
     });
 
-    // 1. searchValue in lastIndexOf has special meaning
-    describe('String', function() {
-        it('Normal Operations', function() {
-            var s = 'Hello, JavaScript!';
-            assert.equal(s.charAt(4), 'o', 'index begins from 0');
-            assert.equal(s.charAt(100), '', 'empty string is returned if index is invalid');
-            assert.equal(s.indexOf('o,'), 4);
-            assert.equal(s.indexOf('ss'), -1);
-            assert.equal(s.lastIndexOf('a'), 10);
-            assert.equal(s.lastIndexOf('', 3), 3, 'if searchValue is empty string, the fromIndex is returned');
-        });
+    it('Convered from string', function () {
+      expect(Number.parseInt('5.6'), 'numbers after dot are omitted').to.be.equal(5);
+      expect(Number.parseInt(' 5 '), 'leading spaces are ignored').to.be.equal(5);
+      expect(Number.parseInt('ab', 16), 'hexdecimal parsing').to.be.equal(0xab);
+      expect(Number.parseInt('ab')).to.be.NaN;        // jshint ignore: line
+      expect(Number.parseInt, 'global parseInt is alias of Number.parseInt').to.be.equal(parseInt);
 
-        it('Split and Join', function() {
-            // TODO RegExp
-        });
-
-        it('Slice', function() {
-            var s = 'Hello, JavaScript!';
-            assert.equal(s.substr(2, 3), 'llo');
-            assert.equal(s.substr(-5), 'ript!', 'negative index starts counting from the end of the string');
-            assert.equal(s.substring(2, 5), 'llo', 'indexEnd is exlcuded');
-            assert.equal(s.substring(5, 2), 'llo', 'if indexStart is greater than indexEnd, two indexes were swapped');
-            assert.equal(s.substring(2, 2), '');
-            assert.equal(s.slice(), 'Hello, JavaScript!', 'a simple copy method');
-        });
-
-        // 1. null is object while undefined id undefined
-        // 2. prefer null when unsure
-        it('The null and undefined', function() {
-            assert.equal(typeof(null), 'object');
-            assert.equal(typeof(undefined), 'undefined');
-        });
-
-        it('Template String(ES6 feature)', function() {
-            var a = 1, b = 1.2, c = 'ljatsh';
-            assert.equal(`${a}_${b}_${c}`, '1_1.2_ljatsh');
-        });
-
-        it('Unicode', function() {
-            var s = '\u4e2d\u534e\u4eba\u6c11\u5171\u548c\u56fd';
-            assert.equal(s, '中华人民共和国');
-
-            assert.equal(String.fromCharCode(0x4e2d, 0x534e, 0x4eba, 0x6c11, 0x5171, 0x548c, 0x56fd), '中华人民共和国');
-            assert.equal(String.fromCodePoint(0x4e2d, 0x534e, 0x4eba, 0x6c11, 0x5171, 0x548c, 0x56fd), '中华人民共和国');
-        });
+      expect(Number.parseFloat('1.5')).to.be.equal(1.5);
+      expect(Number.parseFloat('1.5a'), 'first non digit character and remain ones are omitted').to.be.equal(1.5);
+      expect(Number.parseFloat, 'global parseFloat is alias of Number.parseFloat').to.be.equal(parseFloat);
     });
+
+    it('Converts to string', function () {
+      expect((16.7).toString(), 'default radix is 10').to.be.equal('16.7');
+      expect((16).toString(16), 'hexdecimal format').to.be.equal('10');
+      expect((-16).toString(2), 'binary format').to.be.equal('-10000');
+    });
+
+    it('The valueOf', function () {
+      var numObj = new Number(11); // jshint ignore: line
+      var num = numObj.valueOf();
+      expect(num).to.be.equal(11);
+      // expect(numObj).to.be.an('number');
+      // expect(num).to.be.a('number');
+      // TODO typeof(numObj) == 'object'; typeof constructor and https://github.com/chaijs/type-detect
+    });
+  });
+
+  // 1. undefined, null, NaN, 0, '', false is falsy. Anything else is truthy
+  describe('Boolean', function () {
+    it('The falsy and truthy', function () {
+      var falsy_vars = [undefined, null, NaN, 0, '', false];
+
+      falsy_vars.forEach(function(value) {
+        expect(value).to.be.not.ok;           // jshint ignore: line
+      });
+
+      expect(new Boolean(false), 'any object is truthy, including objects whose valueOf returns false').to.be.ok; // jshint ignore: line
+      expect([], 'array is truthy, event it is empty').to.be.ok;       // jshint ignore: line
+      expect(' ', 'non empty string is truthy').to.be.ok;              // jshint ignore: line
+      expect('false').to.be.ok;                                        // jshint ignore: line
+    });
+  });
+
+  // 1. searchValue in lastIndexOf has special meaning
+  describe('String', function () {
+    it('Normal Operations', function () {
+      var s = 'Hello, JavaScript!';
+      expect(s.charAt(4), 'index begins from 0').to.be.equal('o');
+      expect(s.charAt(100), 'empty string is returned if index is invalid').to.be.empty;     // jshint ignore: line
+      expect(s.indexOf('o,')).to.be.equal(4);
+      expect(s.indexOf('ss')).to.be.equal(-1);
+      expect(s.lastIndexOf('a')).to.be.equal(10);
+      expect(s.lastIndexOf('', 3), 'if searchValue is empty string, the fromIndex is returned').to.be.equal(3);
+    });
+
+    it('Split and Join', function () {
+      // TODO RegExp
+    });
+
+    it('Slice', function () {
+      var s = 'Hello, JavaScript!';
+      expect(s.substr(2, 3)).to.be.equal('llo');
+      expect(s.substr(-5), 'negative index starts counting from the end of the string').to.be.equal('ript!');
+      expect(s.substring(2, 5), 'indexEnd is exlcuded').to.be.equal('llo');
+      expect(s.substring(5, 2), 'if indexStart is greater than indexEnd, two indexes were swapped').to.be.equal('llo');
+      expect(s.substring(2, 2)).to.be.empty;        // jshint ignore: line
+      expect(s.slice(), 'a simple copy method').to.be.equal('Hello, JavaScript!');
+    });
+
+    // 1. null is object while undefined id undefined
+    // 2. prefer null when unsure
+    it('The null and undefined', function () {
+      expect(typeof (null)).to.be.equal('object');
+      expect(typeof (undefined)).to.be.equal('undefined');
+    });
+
+    it('Template String(ES6 feature)', function () {
+      var a = 1, b = 1.2, c = 'ljatsh';
+      expect(`${a}_${b}_${c}`).to.be.equal('1_1.2_ljatsh');
+    });
+
+    it('Unicode', function () {
+      var s = '\u4e2d\u534e\u4eba\u6c11\u5171\u548c\u56fd';
+      expect(s).to.be.equal('中华人民共和国');
+
+      expect(String.fromCharCode(0x4e2d, 0x534e, 0x4eba, 0x6c11, 0x5171, 0x548c, 0x56fd)).to.be.equal('中华人民共和国');
+      expect(String.fromCodePoint(0x4e2d, 0x534e, 0x4eba, 0x6c11, 0x5171, 0x548c, 0x56fd)).to.be.equal('中华人民共和国');
+    });
+  });
 });
