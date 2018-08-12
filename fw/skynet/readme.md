7e1fe69f5eeac1447e2e708d8cc5339dcc4e1c92
lj@sh <longjun.wang@qq.com>
2018-08-12 16:11:39 +0800

    skynet mysql test

--- fw/skynet/readme.md
+++ fw/skynet/readme.md
@@ -3,4 +3,5 @@ SKYNET
 
 * http请求，如果超时，则请求co还是会继续等待；这不是个优雅的处理方式
 * http,dns出错的处理，比较粗暴，我更青睐返回值方式
+* http的SSL支持，参考[这里](https://github.com/dpull/lua-webclient)
 * skynet.exit()貌似会等待所有co结束才出来
--- fw/skynet/spec/skynet_spec.lua
+++ fw/skynet/spec/skynet_spec.lua
@@ -2,6 +2,7 @@
 local skynet = require('skynet')
 local dns = require('skynet.dns')
 local httpc = require('http.httpc')
+local mysql = require('skynet.db.mysql')
 local handler = require('test_handler')
 
 describe('skynet', function()
@@ -113,5 +114,135 @@ describe('skynet', function()
     httpc.timeout = 10 -- 0.1 seconds
     assert.has.error(function() httpc.get('8.8.8.8', '/', response_headers) end)
     assert.has.no.error(function() pcall(httpc.get, '8.8.8.8', '/', response_headers) end)
+  end)
+end)
+
+describe('skynet mysql', function()
+  local self = {}
+  local mysql_ip = '172.17.0.6'
+
+  local function connect_db(ip, db, compact_array, cb)
+    return mysql.connect{
+      host = ip,
+      port = 3306,
+      database = db,
+      user = 'root',
+      password = 'root',
+      max_packet_size = 1024 * 1024,
+      compact_arrays = compact_array or false,
+      on_connect = cb
+    }
+  end
+
+  local function dump(obj)
+    local getIndent, quoteStr, wrapKey, wrapVal, dumpObj
+    getIndent = function(level)
+        return string.rep("\t", level)
+    end
+    quoteStr = function(str)
+        return '"' .. string.gsub(str, '"', '\\"') .. '"'
+    end
+    wrapKey = function(val)
+        if type(val) == "number" then
+            return "[" .. val .. "]"
+        elseif type(val) == "string" then
+            return "[" .. quoteStr(val) .. "]"
+        else
+            return "[" .. tostring(val) .. "]"
+        end
+    end
+    wrapVal = function(val, level)
+        if type(val) == "table" then
+            return dumpObj(val, level)
+        elseif type(val) == "number" then
+            return val
+        elseif type(val) == "string" then
+            return quoteStr(val)
+        else
+            return tostring(val)
+        end
+    end
+    dumpObj = function(obj, level)
+        if type(obj) ~= "table" then
+            return wrapVal(obj)
+        end
+        level = level + 1
+        local tokens = {}
+        tokens[#tokens + 1] = "{"
+        for k, v in pairs(obj) do
+            tokens[#tokens + 1] = getIndent(level) .. wrapKey(k) .. " = " .. wrapVal(v, level) .. ","
+        end
+        tokens[#tokens + 1] = getIndent(level - 1) .. "}"
+        return table.concat(tokens, "\n")
+    end
+    return dumpObj(obj, 0)
+  end
+
+  setup(function()
+    local t = {cb = function(...) end}
+    spy.on(t, 'cb')
+    local db = connect_db(mysql_ip, nil, false, t.cb)
+    assert.is.table(db)
+    assert.spy(t.cb).was.called(1)      -- connected callback should be called
+
+    db:query(
+      'DROP DATABASE IF EXISTS skynet_test;' ..
+      'CREATE DATABASE skynet_test DEFAULT CHARACTER SET utf8;' ..
+      'USE skynet_test;' ..
+      'CREATE TABLE t1(name VARCHAR(24) NOT NULL, age TINYINT UNSIGNED DEFAULT 0);'
+    )
+  end)
+
+  teardown(function()
+    local db = connect_db(mysql_ip, 'skynet_test')
+    db:query('DROP DATABASE skynet_test')
+
+    db:disconnect()
+  end)
+
+  before_each(function()
+    self.db = connect_db(mysql_ip, 'skynet_test')
+    assert.is.table(self.db)
+
+    self.db:query('DELETE FROM t1;')
+  end)
+
+  after_each(function()
+    self.db:query('DELETE FROM t1;')
+    self.db:disconnect()
+  end)
+
+  it('normal usage scenario', function()
+    local res = self.db:query("INSERT INTO t1 VALUES ('ljatsh', 34), ('ljatbj', 29);")
+    assert.are.same(2, res.affected_rows)
+
+    res = self.db:query('SELECT * FROM t1 ORDER BY age DESC;')
+    assert.are.same({{name='ljatsh', age=34}, {name='ljatbj', age=29}}, res)
+  end)
+
+  it('compact array', function()
+    local db = connect_db(mysql_ip, 'skynet_test', true)
+
+    local res = db:query("INSERT INTO t1 VALUES ('ljatsh', 34), ('ljatbj', 29);")
+    assert.are.same(2, res.affected_rows)
+
+    res = db:query('SELECT * FROM t1 ORDER BY age DESC;')
+    assert.are.same(res, {{'ljatsh', 34}, {'ljatbj', 29}})
+  end)
+
+  it('multiple results', function()
+    local res = self.db:query("INSERT INTO t1 VALUES ('ljatsh', 34), ('ljatbj', 29), ('ljatxa', '18')")
+    assert.are.same(3, res.affected_rows)
+
+    local res = self.db:query(
+      'SELECT * FROM t1 WHERE age=18;' ..
+      'SELECT * FROM t1 WHERE age=29;' ..
+      'SELECT * FROM t1 WHERE age=34;'
+    )
+
+    assert.is.truthy(res.mulitresultset)
+    assert.are.same({{name='ljatxa', age=18}}, res[1])
+    assert.are.same({{name='ljatbj', age=29}}, res[2])
+    assert.are.same({{name='ljatsh', age=34}}, res[3])
   end)
 end)
