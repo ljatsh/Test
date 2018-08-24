a4f8ad583f259921ab413f2af15c47cb9a301195
ljatsh <longjun.wang@qq.com>
2018-08-24 09:16:31 +0800

    several c helper functions test

--- lua/src/lua53/spec/extension_spec.lua
+++ lua/src/lua53/spec/extension_spec.lua
@@ -5,14 +5,29 @@ describe('extension', function()
   setup(function()
     self.cpath = package.cpath
     package.cpath = package.cpath .. ';./test/?.so'
+
+    self.test = require('libtest')
   end)
 
   teardown(function()
     package.cpath = self.cpath
   end)
 
-  it('test', function()
-    local test = require('libtest')
-    assert.are.same(3.0, test.sum(1.4, 1.6))
+  it('map', function()
+    local t = {1, 5, 9}
+    self.test.map(t, function(v) return v * 3 end)
+    assert.are.same({3, 15, 27}, t)
+
+    assert.has.error.match(function() self.test.map() end, 'table expected, got no value')
+    assert.has.error.match(function() self.test.map({}, 1) end, 'function expected, got number')
+    assert.has.error(function() self.test.map({1}, function() assert(false) end) end)
+  end)
+
+  it('split', function()
+    -- elment count == count of delimiters + 1
+    assert.are.same({'', ''}, self.test.split(';', ';'))
+    assert.are.same({''}, self.test.split('\r\n\t ', ';'))
+    assert.are.same({'1', '3', '1 2', '4', ''}, self.test.split(' 1 ; 3 ; 1 2 ; 4;', ';'))
+    assert.are.same({'1', '3', '1 2', '4', ''}, self.test.split(' 1 $$ 3 $$ 1 2 $$ 4$$', '$$'))
   end)
 end)
--- lua/src/lua53/test/libtest.c
+++ lua/src/lua53/test/libtest.c
@@ -1,6 +1,38 @@
 
+#include <string.h>
+#include <ctype.h>
 #include <lua.h>
 #include <lauxlib.h>
+
+void
+push_lstring_with_space_trimed(lua_State *L, const char *s, int len) {
+  if (len == 0) {
+    lua_pushstring(L, "");
+    return;
+  }
+
+  const char* end = s + len - 1;
+
+  // skip left space
+  while (isspace(*s))
+    s++;
+
+  // skip right space
+  while (isspace(*end))
+    end--;
+
+  if (s > end) {
+    lua_pushstring(L, "");
+    return;
+  }
+
+  lua_pushlstring(L, s, (int)(end - s + 1));
+}
+
+void
+push_string_with_space_trimed(lua_State *L, const char *s) {
+  push_lstring_with_space_trimed(L, s, strlen(s));
+}
 
 static int
 sum(lua_State *L) {
@@ -11,8 +43,59 @@ sum(lua_State *L) {
   return 1;
 }
 
+static int
+map(lua_State *L) {
+  // 1st parameter is table
+  luaL_checktype(L, 1, LUA_TTABLE);
+  // 2nd parameter is function
+  luaL_checktype(L, 2, LUA_TFUNCTION);
+
+  int len = luaL_len(L, 1);
+  for (int i=1; i<=len; i++) {
+    lua_pushvalue(L, 2);           // t, f, f
+    lua_geti(L, 1, i);        // t, f, f, v
+    lua_call(L, 1, 1);       // t, f, 1
+    lua_seti(L, 1, i);        // t, f
+  }
+
+  return 0;
+}
+
+static int
+split(lua_State *L) {
+  const char* s = luaL_checkstring(L, 1);
+  const char* delimiter = luaL_checkstring(L, 2);
+  int deli_len = strlen(delimiter);
+
+  const char* p = s;
+  const char* end = s;
+  lua_newtable(L);        // s, delimiter, t
+  int i = 1;
+
+  do {
+    end = strstr(p, delimiter);
+
+    if (end == NULL) {
+      push_string_with_space_trimed(L, p);
+    }
+    else {
+      push_lstring_with_space_trimed(L, p, (int)(end - p));
+      p = end + deli_len;
+    }
+
+    lua_seti(L, 3, i++);
+    if (end == NULL)
+      break;
+
+  } while (1);
+
+  return 1;
+}
+
 static const struct luaL_Reg mylib[] = {
   {"sum", sum},
+  {"map", map},
+  {"split", split},
   {NULL, NULL}
 };
 
