6c76d72167fe84b4e1b029dea100099f84570cb8
lj@sh <longjun.wang@qq.com>
2018-09-03 21:00:22 +0800

    refactor http client

--- lua/src/lua51/http/client.lua
+++ lua/src/lua51/http/client.lua
@@ -1,5 +1,7 @@
 
-local sock = require('socket')
+--- http client
+
+local stream_socket = require('stream_socket')
 local headers = require('http.header')
 local hreq = require('http.request')
 local hresp = require('http.response')
@@ -8,18 +10,25 @@ local lhp = require('http.parser')
 
 local M_ = {}
 
----
--- @returns response | nil, err
-function M_.request(req, host, port)
+--- perform http request
+-- @param req http request
+-- @param host
+-- @param port
+-- @param on_body if on_body is not nil, body data will not be cached
+-- @returns response
+-- @returns body(error if response is nil)
+function M_.do_http(req, host, port, on_body)
   port = port or 80
-  req.headers[headers.Host] = host
+  --req.headers[headers.Host] = host
 
-  -- write
-  local socket, err = sock.connect(host, port)
-  if socket == nil then
+  -- connect
+  local socket = stream_socket.new(host, port)
+  local ret, err = stream_socket:connect()
+  if ret == nil then
     return nil, err
   end
 
+  -- write
   local r, err = socket:write(tostring(req))
   if not r then
     return nil, err
@@ -27,49 +36,65 @@ function M_.request(req, host, port)
 
   -- read & parse
   local response = hresp.new()
-  local bodies = {}
-  local finished = false
+  local finish_header = false
+  local finish_message = false
 
-  local parser
-  parser = lhp.response {
+  local t =  {
     on_header = function(hkey, hval)
-      response.headers[hkey] = hval
+      response.headers:set(hkey, hval)
     end,
 
     on_headers_complete = function()
       response.status = parser:status_code()
       response.version_major, response.version_minor = parser:version()
+      finish_header = true
 
-      print('on_headers_complete')
-
-      print(parser:method())
-
-      if req:method() == 'HEAD' then
-        finished = true
-      end
-    end,
-
-    on_body = function(body)
-      if body ~= nil then
-        bodies[#bodies + 1] = body
-      end
+      -- if req:method() == 'HEAD' then
+      --   finished = true
+      -- end
     end,
 
     on_message_complete = function()
-      finished = true
-    end
+      finish_message = true
+    end,
+
+    on_body = on_body
   }
+
+  if t.on_body == nil then
+    local bodies = {}
+    t.on_body = function(body)
+      if body ~= nil then
+        bodies[#bodies + 1] = body
+      end
+    end
+  end
+
+  local parser = lhp.response(t)
 
   local count
   local data
   repeat
     count, data = socket:read()
-    print(count)
+
+    -- error occurred
     if count == false then
-      -- for connection-close
-      parser:execute('')
       break
     end
+
+    -- EOF occurred
+    if count == nil then
+      -- check Connection: close
+      if finish_header and string.lower(response.headers:get(headers.Connection) or '') == 'close' then
+        count, data = parser:execute('')
+      else
+        count, data = false, 'connection closed by peer'
+      end
+
+      break
+    end
+
+    
 
     parser:execute(data)
 
