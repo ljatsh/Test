c1e62f44b9f04b6aa17be1de0e52f6b8769985fa
lj@sh <longjun.wang@qq.com>
2019-07-11 20:59:07 +0800

    tool xls2tbl - python and xml output sample

---
+++ tools/xls2tbl/Test/TestPython.cs
@@ -0,0 +1,83 @@
+// # -*- coding:utf-8 -*-
+
+
+// class _CC(object):
+//     """
+//     Auto Generated Python Class.
+//     """
+
+//     def __init__(self, **kwargs):
+//         self._key = kwargs['key']
+//         self._value = kwargs['value']
+
+//     @property
+//     def key(self):
+//         return self._key
+
+//     @property
+//     def value(self):
+//         return self._value
+
+//     def __str__(self):
+//         infos = []
+//         infos.append('key=%s' % str(self._key))
+//         infos.append('value=%s' % str(self._value))
+//         return '; '.join(infos) 
+
+// CC = {}
+// CC['timelimit_jiaopai'] = _CC(key='timelimit_jiaopai', value='15.0')
+// CC['timelimit_fapai'] = _CC(key='timelimit_fapai', value='3.0')
+// CC['timelimit_qiangpai'] = _CC(key='timelimit_qiangpai', value='10.0')
+// CC['timelimit_double'] = _CC(key='timelimit_double', value='9.0')
+// CC['timelimit_ai_use_card'] = _CC(key='timelimit_ai_use_card', value='0.5')
+// CC['timelimit_use_card'] = _CC(key='timelimit_use_card', value='25.0')
+// CC['timelimit_skip'] = _CC(key='timelimit_skip', value='5.0')
+// CC['fapai_again_wait_duration'] = _CC(key='fapai_again_wait_duration', value='0.5')
+// CC['timelimit_wait_online_withcard'] = _CC(key='timelimit_wait_online_withcard', value='3600.0')
+// CC['init_game_coin'] = _CC(key='init_game_coin', value='10000.0')
+// CC['sys_give_game_coin_count'] = _CC(key='sys_give_game_coin_count', value='3000.0')
+// CC['game_coin_give_trigger_count'] = _CC(key='game_coin_give_trigger_count', value='2000.0')
+// CC['game_coin_give_times_every_day'] = _CC(key='game_coin_give_times_every_day', value='2.0')
+// CC['share_continue_wins'] = _CC(key='share_continue_wins', value='3.0')
+// CC['share_multi_line'] = _CC(key='share_multi_line', value='1500.0')
+// CC['online_mission_start'] = _CC(key='online_mission_start', value='20.0')
+// CC['online_mission_end'] = _CC(key='online_mission_end', value='22.0')
+// CC['change_name_cost'] = _CC(key='change_name_cost', value='300')
+
+
+// class _CardTypeAnim(object):
+//     """
+//     Auto Generated Python Class.
+//     """
+
+//     def __init__(self, **kwargs):
+//         self._card_type = kwargs['card_type']
+//         self._duration = kwargs['duration']
+
+//     @property
+//     def card_type(self):
+//         return self._card_type
+
+//     @property
+//     def duration(self):
+//         return self._duration
+
+//     def __str__(self):
+//         infos = []
+//         infos.append('card_type=%s' % str(self._card_type))
+//         infos.append('duration=%s' % str(self._duration))
+//         return '; '.join(infos) 
+
+// CardTypeAnim = {}
+// CardTypeAnim[1] = _CardTypeAnim(card_type=1, duration=2.3)
+// CardTypeAnim[2] = _CardTypeAnim(card_type=2, duration=1.7)
+// CardTypeAnim[3] = _CardTypeAnim(card_type=3, duration=0.0)
+// CardTypeAnim[4] = _CardTypeAnim(card_type=4, duration=0.0)
+// CardTypeAnim[5] = _CardTypeAnim(card_type=5, duration=0.0)
+// CardTypeAnim[6] = _CardTypeAnim(card_type=6, duration=0.0)
+// CardTypeAnim[7] = _CardTypeAnim(card_type=7, duration=0.0)
+// CardTypeAnim[8] = _CardTypeAnim(card_type=8, duration=0.0)
+// CardTypeAnim[9] = _CardTypeAnim(card_type=9, duration=0.0)
+// CardTypeAnim[10] = _CardTypeAnim(card_type=10, duration=0.0)
+// CardTypeAnim[11] = _CardTypeAnim(card_type=11, duration=1.9)
+// CardTypeAnim[12] = _CardTypeAnim(card_type=12, duration=0.0)
---
+++ tools/xls2tbl/Test/TestXml.cs
@@ -0,0 +1,178 @@
+// <?xml version='1.0' encoding='UTF-8'?>
+// <CC>
+//   <info key="timelimit_jiaopai">
+//     <value>15.0</value>
+//   </info>
+//   <info key="timelimit_fapai">
+//     <value>3.0</value>
+//   </info>
+//   <info key="timelimit_qiangpai">
+//     <value>10.0</value>
+//   </info>
+//   <info key="timelimit_double">
+//     <value>9.0</value>
+//   </info>
+//   <info key="timelimit_ai_use_card">
+//     <value>0.5</value>
+//   </info>
+//   <info key="timelimit_use_card">
+//     <value>25.0</value>
+//   </info>
+//   <info key="timelimit_skip">
+//     <value>5.0</value>
+//   </info>
+//   <info key="fapai_again_wait_duration">
+//     <value>0.5</value>
+//   </info>
+//   <info key="timelimit_wait_online_withcard">
+//     <value>3600.0</value>
+//   </info>
+//   <info key="init_game_coin">
+//     <value>10000.0</value>
+//   </info>
+//   <info key="sys_give_game_coin_count">
+//     <value>3000.0</value>
+//   </info>
+//   <info key="game_coin_give_trigger_count">
+//     <value>2000.0</value>
+//   </info>
+//   <info key="game_coin_give_times_every_day">
+//     <value>2.0</value>
+//   </info>
+//   <info key="share_continue_wins">
+//     <value>3.0</value>
+//   </info>
+//   <info key="share_multi_line">
+//     <value>1500.0</value>
+//   </info>
+//   <info key="online_mission_start">
+//     <value>20.0</value>
+//   </info>
+//   <info key="online_mission_end">
+//     <value>22.0</value>
+//   </info>
+//   <info key="change_name_cost">
+//     <value>300</value>
+//   </info>
+// </CC>
+
+
+// <?xml version='1.0' encoding='UTF-8'?>
+// <MailTemplate>
+//   <info id="1">
+//     <sender_name>STRID_Mail_Sender1</sender_name>
+//     <title>STRID_Mail_Welcome</title>
+//     <content>STRID_Mail_Welcome_Content</content>
+//   </info>
+//   <info id="2">
+//     <sender_name>STRID_Mail_Sender2</sender_name>
+//     <title>STRID_Mail_Vip_Platinum_Daily_Reward</title>
+//     <content>STRID_Mail_Vip_Platinum_Daily_Reward_Content</content>
+//   </info>
+//   <info id="3">
+//     <sender_name>STRID_Mail_Sender3</sender_name>
+//     <title>STRID_Mail_Vip_Gold_Daily_Reward</title>
+//     <content>STRID_Mail_Vip_Gold_Daily_Reward_Content</content>
+//   </info>
+//   <info id="4">
+//     <sender_name>STRID_Mail_Sender4</sender_name>
+//     <title>STRID_Mail_Vip_Diamond_Daily_Reward</title>
+//     <content>STRID_Mail_Vip_Diamond_Daily_Reward_Content</content>
+//   </info>
+//   <info id="5">
+//     <sender_name>STRID_Mail_Sender5</sender_name>
+//     <title>STRID_Mail_Chart_1</title>
+//     <content>STRID_Mail_Chart_Content_1</content>
+//   </info>
+//   <info id="6">
+//     <sender_name>STRID_Mail_Sender6</sender_name>
+//     <title>STRID_Mail_Chart_2</title>
+//     <content>STRID_Mail_Chart_Content_2</content>
+//   </info>
+//   <info id="7">
+//     <sender_name>STRID_Mail_Sender7</sender_name>
+//     <title>STRID_Mail_Chart_3</title>
+//     <content>STRID_Mail_Chart_Content_3</content>
+//   </info>
+//   <info id="8">
+//     <sender_name>STRID_Mail_Sender8</sender_name>
+//     <title>STRID_Mail_Chart_4</title>
+//     <content>STRID_Mail_Chart_Content_4</content>
+//   </info>
+//   <info id="9">
+//     <sender_name>STRID_Mail_Sender9</sender_name>
+//     <title>STRID_Mail_Chart_5</title>
+//     <content>STRID_Mail_Chart_Content_5</content>
+//   </info>
+//   <info id="10">
+//     <sender_name>STRID_Mail_Sender10</sender_name>
+//     <title>STRID_Mail_Chart_6</title>
+//     <content>STRID_Mail_Chart_Content_6</content>
+//   </info>
+//   <info id="11">
+//     <sender_name>STRID_Mail_Sender11</sender_name>
+//     <title>STRID_Mail_Chart_7</title>
+//     <content>STRID_Mail_Chart_Content_7</content>
+//   </info>
+//   <info id="12">
+//     <sender_name>STRID_Mail_Sender12</sender_name>
+//     <title>STRID_Mail_Chart_8</title>
+//     <content>STRID_Mail_Chart_Content_8</content>
+//   </info>
+//   <info id="13">
+//     <sender_name>STRID_Mail_Sender13</sender_name>
+//     <title>STRID_Mail_Chart_9</title>
+//     <content>STRID_Mail_Chart_Content_9</content>
+//   </info>
+//   <info id="14">
+//     <sender_name>STRID_Mail_Sender14</sender_name>
+//     <title>STRID_Mail_Chart_10</title>
+//     <content>STRID_Mail_Chart_Content_10</content>
+//   </info>
+//   <info id="101">
+//     <sender_name>STRID_Mail_Sender101</sender_name>
+//     <title>STRID_Mail_Chart_101</title>
+//     <content>STRID_Mail_Chart_Content_101</content>
+//   </info>
+//   <info id="111">
+//     <sender_name>STRID_Mail_Sender111</sender_name>
+//     <title>STRID_Mail_Chart_111</title>
+//     <content>STRID_Mail_Chart_Content_111</content>
+//   </info>
+//   <info id="112">
+//     <sender_name>STRID_Mail_Sender112</sender_name>
+//     <title>STRID_Mail_Chart_112</title>
+//     <content>STRID_Mail_Chart_Content_112</content>
+//   </info>
+//   <info id="113">
+//     <sender_name>STRID_Mail_Sender113</sender_name>
+//     <title>STRID_Mail_Chart_113</title>
+//     <content>STRID_Mail_Chart_Content_113</content>
+//   </info>
+//   <info id="114">
+//     <sender_name>STRID_Mail_Sender114</sender_name>
+//     <title>STRID_Mail_Chart_114</title>
+//     <content>STRID_Mail_Chart_Content_114</content>
+//   </info>
+//   <info id="115">
+//     <sender_name>STRID_Mail_Sender115</sender_name>
+//     <title>STRID_Mail_Chart_115</title>
+//     <content>STRID_Mail_Chart_Content_115</content>
+//   </info>
+//   <info id="116">
+//     <sender_name>STRID_Mail_Sender116</sender_name>
+//     <title>STRID_Mail_Chart_116</title>
+//     <content>STRID_Mail_Chart_Content_116</content>
+//   </info>
+//   <info id="117">
+//     <sender_name>STRID_Mail_Sender117</sender_name>
+//     <title>STRID_Mail_Chart_117</title>
+//     <content>STRID_Mail_Chart_Content_117</content>
+//   </info>
+//   <info id="118">
+//     <sender_name>STRID_Mail_Sender118</sender_name>
+//     <title>STRID_Mail_Chart_118</title>
+//     <content>STRID_Mail_Chart_Content_118</content>
+//   </info>
+// </MailTemplate>
+
