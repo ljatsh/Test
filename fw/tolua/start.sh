310222d316aa5b1433d789481f830aeb05ca8644
ljatsh <longjun.wang@qq.com>
2019-11-01 08:30:50 +0800

    tolua

--- fw/tolua/start.sh
+++ fw/tolua/start.sh
@@ -2,4 +2,6 @@
 
 PWD=$(cd `dirname $0` && pwd)
 
-docker run -it --name tolua --rm -v $PWD:/opt/dev -w /opt/dev tolua-dev
+docker run -it --name tolua --rm -v $PWD:/opt/dev -w /opt/dev \
+ --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+ tolua-dev
