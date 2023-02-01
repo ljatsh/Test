4919cecced1e87ffdbd8bd677d9c867a46a6766f
lj@sh <longjun.wang@qq.com>
2023-02-01 15:34:21 +0800

    js

--- js/src/book/learning_javascript_3rd_edition/cp5.js
+++ js/src/book/learning_javascript_3rd_edition/cp5.js
@@ -3,38 +3,42 @@
 // Chapter 5. Expressions and Operators
 
 var assert = require('assert');
+const { expect } = require('chai');
 
 describe('Expressions and Operators', function() {
-    // 1. Object destructuring can be done on assignment alone, but is must be surrounded by parentheses.
-    // 2. Array destructuring is less power than Python. var [a, b, ...c, d] = [3, 6, 9, 10, 11, 12] is forbidden.
-    // 3. Array destructuring make it easy to swap the values of two variables
-    // 4. Perfer === and !== to avoid unexpected behavior
-    describe('Destructuring Assignment', function() {
-        it('Object Destructuring', function() {
-            var obj = {a : 1, b : 'ljatsh'};
-            var {a, b, c} = obj;
-            assert.equal(a, 1);
-            assert.equal(b, 'ljatsh');
-            assert.equal(c, undefined);
+  // 1. Object destructuring can be done on assignment alone, but is must be surrounded by parentheses.
+  // 2. Array destructuring is less power than Python. var [a, b, ...c, d] = [3, 6, 9, 10, 11, 12] is forbidden.
+  // 3. Array destructuring make it easy to swap the values of two variables
+  // 4. Perfer === and !== to avoid unexpected behavior
+  describe('Destructuring Assignment', function() {
+    it('Object Destructuring', function() {
+      var obj = {a : 1, b : 'ljatsh'};
+      var {a, b, c} = obj;
+      expect(a).to.be.equal(1);
+      expect(b).to.be.equal('ljatsh');
+      //expect(c).to.be.undefined;
+      expect(c).to.be.equal(undefined);
 
-            // TODO: There are SyntaxError if omit semicolon here.
-            // Add semicolon after every block, thouth it is not necessary.
-            ({c, a, b} = obj);
-            assert.equal(a, 1);
-            assert.equal(b, 'ljatsh');
-            assert.equal(c, undefined);
-        });
+      // TODO: There are SyntaxError if omit semicolon here.
+      // Add semicolon after every block, thouth it is not necessary.
+      ({c, a, b} = obj);
+      expect(a).to.be.equal(1);
+      expect(b).to.be.equal('ljatsh');
+      //expect(c).to.be.undefined();
+      expect(c).to.be.equal(undefined);
+    });
 
-        it('Array Destructuring', function() {
-            var [x, y, ...rest] = [3, 6, 9, 10, 11, 12];
-            assert.equal(x, 3);
-            assert.equal(y, 6);
-            assert.deepEqual(rest, [9, 10, 11, 12]);
+    it('Array Destructuring', function() {
+      var [x, y, ...rest] = [3, 6, 9, 10, 11, 12];
+      expect(x).to.be.equal(3);
+      expect(y).to.be.equal(6);
+      expect(rest).be.deep.equal([9, 10, 11, 12]);
+      //assert.deepEqual(rest, [9, 10, 11, 12]);
 
-            [x, y] = [y, x];
-            assert.equal(x, 6);
-            assert.equal(y, 3);
-        });
+      [x, y] = [y, x];
+      expect(x).to.be.equal(6);
+      expect(y).to.be.equal(3);
+    });
 
         it('Equality and Inequality Operators', function() {
             // strict equality
---
+++ js/src/readme.md
@@ -0,0 +1,10 @@
+
+JavaScript Specification
+========================
+
+1. [Destructuring Assignment](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
+
+TODO
+====
+
+1. chai undefined assertion -> cp5.js
